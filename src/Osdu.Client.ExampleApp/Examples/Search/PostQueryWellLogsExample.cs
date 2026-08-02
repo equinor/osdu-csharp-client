1cc6753df1c68d85d202c33d213a423dd16604c6
Mohammad Rehan Khan (Bouvet Norge AS) <MRK@equinor.com>
2026-08-02 13:13:15 +0200

    fix: added PostQueryWellLogsExample

--- src/Osdu.Client.ExampleApp/Examples/Search/PostQueryWellLogsExample.cs
+++ src/Osdu.Client.ExampleApp/Examples/Search/PostQueryWellLogsExample.cs
@@ -3,17 +3,20 @@ using Osdu.Client.Apis;
 using Osdu.Client.Apis.Search;
 using Osdu.Client.ExampleApp.ExamplesBuilder;
 using Osdu.Client.ExampleApp.Extensions;
+using Osdu.Client.Extensions;
+using Osdu.Client.Schemas.MasterData;
+using Osdu.Client.Schemas.WorkProductComponent;
 
 namespace Osdu.Client.ExampleApp.Examples;
 
-public class SearchWellLogsExample(IOsduClient osduClient) : ExampleBase
+public class PostQueryWellLogsExample(IOsduClient osduClient) : ExampleBase
 {
     public override string Category => ExampleCategory.Search;
     public override string Text => $"{Category}.{GetType().Name.RemoveExample()}";
-    public override string ShortDescription => $"This is an example for 'OsduClient.{Text}' api endpoint";
+    public override string ShortDescription => $"This is an example for 'OsduClient.Search.PostQuery' api endpoint with strongly-typed 'Wellbore_1_3_0' result";
 
     [ExampleParameter(DisplayName = "Kind", Required = true, Order = 0, Description = "The kind of WellLog record to search for.")]
-    public string Kind { get; set; } = "osdu:wks:work-product-component--WellLog:*";
+    public string Kind { get; set; } = "osdu:wks:work-product-component--WellLog:1.3.0";
 
     [ExampleParameter(DisplayName = "Limit", Order = 1, Description = "Maximum number of results to return.")]
     public int Limit { get; set; } = 10;
@@ -33,6 +36,10 @@ public class SearchWellLogsExample(IOsduClient osduClient) : ExampleBase
 
         var response = await osduClient.Search.PostQueryAsync(request, cancellationToken);
 
-        return JsonSerializer.Serialize(response, new JsonSerializerOptions { WriteIndented = true });
+        IEnumerable<WellLog_1_3_0> wellbores = response.Results.DeserializeList<WellLog_1_3_0>();
+
+        return JsonSerializer.Serialize(wellbores, new JsonSerializerOptions { WriteIndented = true });
+
+        //return JsonSerializer.Serialize(response, new JsonSerializerOptions { WriteIndented = true });
     }
 }
