686f00acada76cf8ba55232706cebc2077216d1e
Mohammad Rehan Khan (Bouvet Norge AS) <MRK@equinor.com>
2026-08-12 12:18:31 +0200

    refactor: updated comments

--- src/Osdu.Client.Generator.ConsoleApp/Program.cs
+++ src/Osdu.Client.Generator.ConsoleApp/Program.cs
@@ -9,16 +9,16 @@ internal class Program
     static void Main(string[] args)
     {
         Console.WriteLine($"""
-                          =====================================================================================================
+                          ========================================================================================================================================
                           OSDU Client Generator:                                                                      
                           
-                          A tool for generating C# API clients and Schema models from OSDU api/schema definition (json) files    
+                          A tool for generating C# API clients and Domain Data models classes from OSDU Api & Domain Data OpenApi specification files (*.json)
                           
-                          - API definitions are used to generate strongly-typed api clients,
-                          - Schema definitions are used to generate strongly-typed model classes
+                          - API definitions are used to generate strongly-typed api clients with related request/response model classes,
+                          - Schema definitions are used to generate strongly-typed Domain data model classes
                           
                           The output will be placed in the Osdu.Client project.
-                          =====================================================================================================
+                          ========================================================================================================================================
                           """);
 
         ServiceProvider serviceProvider = ConfigureServices()
